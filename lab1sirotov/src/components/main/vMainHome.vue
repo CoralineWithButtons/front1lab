ce72766dd446b6e38a7970b7f7ce3d61da0e109d
Dmitry <sirotov.d@mail.ru>
2021-12-04 03:13:08 +0300

    first commit

---
+++ lab1sirotov/src/components/main/vMainHome.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="main">
+    <h1 class="main__title">Сайт новостей "Шлёпа ньюс"</h1><br>
+    <img src='https://g2.delphi.lv/images/pix/676x385/1O2BjvVguG8/caracal-gosha-big-floppa-53258705.jpg' class="img">
+    <h2>Только свежие новости от Большого русского кота Шлёпы. Самые правдивые новости, интриги и расследования. Только у Шлёпы!</h2>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.main {
+  justify-content: center;
+  margin-top: 50px;
+  max-width: 750px;
+    margin: 0 auto;
+}
+.img {
+    display: flex;
+   min-height: 10em;
+    vertical-align: middle;
+    display:inline-block;
+}
+</style>
